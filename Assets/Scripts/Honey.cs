26dde62f275b7e920e89c18796e1a999bd8c46e1
José Magalhães <joses.magalhaes@hotmail.com>
2023-12-29 19:44:28 -0300

    :sparkles: feat: Adição de efeitos nos favos de mel

--- Assets/Scripts/Honey.cs
+++ Assets/Scripts/Honey.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class Honey : MonoBehaviour
 {
-
+    public GameObject honeyFx;
+    public AudioClip coinSound;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            Destroy(gameObject);
+            Instantiate(honeyFx, this.gameObject.transform);
+            GameObject soundObject = new GameObject("CollisionSound");
+            AudioSource audioSource = soundObject.AddComponent<AudioSource>();
+            audioSource.clip = coinSound;
+            audioSource.Play();
+            Destroy(soundObject, coinSound.length);
+            Destroy(gameObject, 0.5f);
         }
 
     }
